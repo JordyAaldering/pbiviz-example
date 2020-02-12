9809457bdfbd29ba6c058c46ce1b310bd462a229
Jordy Aaldering <aalderingjordy@gmail.com>
2020-02-12 13:20:43 +0100

    Changed to Bar Graph

--- src/visual.ts
+++ src/visual.ts
@@ -20,6 +20,16 @@ import EnumerateVisualObjectInstancesOptions = powerbi.EnumerateVisualObjectInst
 
 type Selection<T extends d3.BaseType> = d3.Selection<T, any, any, any>;
 
+export interface DataPoint {
+    category: string;
+    value: number;
+};
+
+export interface ViewModel {
+    dataPoints: DataPoint[];
+    maxValue: number;
+};
+
 export class Visual implements IVisual {
     private host: IVisualHost;
     private settings: VisualSettings;
@@ -27,64 +37,113 @@ export class Visual implements IVisual {
 
     private svg: Selection<SVGElement>;
     private container: Selection<SVGElement>;
-    private circle: Selection<SVGElement>;
-    private textValue: Selection<SVGElement>;
-    private textLabel: Selection<SVGElement>;
+    private xAxisContainer: Selection<SVGElement>;
+    private yAxisContainer: Selection<SVGElement>;
 
     constructor(options: VisualConstructorOptions) {
         this.svg = d3.select(options.element)
-            .append('svg')
-            .classed('circleCard', true);
-        this.container = this.svg.append("g")
-            .classed('container', true);
-        this.circle = this.container.append("circle")
-            .classed('circle', true);
-        this.textValue = this.container.append("text")
-            .classed("textValue", true);
-        this.textLabel = this.container.append("text")
-            .classed("textLabel", true);
+            .append("svg")
+            .classed("svg", true);
+
+        this.container = this.svg
+            .append("g")
+            .classed("container", true);
+
+        this.xAxisContainer = this.container
+            .append("g")
+            .classed("container", true);
+        
+        this.yAxisContainer = this.container
+            .append("g")
+            .classed("container", true);
     }
 
     public update(options: VisualUpdateOptions) {
-        let dataView: DataView = options.dataViews[0];
+        let viewModel = this.getViewModel(options);
+        this.visualSettings = VisualSettings.parse<VisualSettings>(options.dataViews[0]);
+
         let width: number = options.viewport.width;
         let height: number = options.viewport.height;
 
-        this.svg.attr("width", width);
-        this.svg.attr("height", height);
-        
-        let radius: number = Math.min(width, height) / 2.2;
-        let fontSizeValue: number = Math.min(width, height) / 5;
-        let fontSizeLabel: number = fontSizeValue / 4;
+        const margin = { top: 40, right: 15, bottom: 40, left: 185 };
+        const innerWidth = width - margin.left - margin.right;
+        const innerHeight = height - margin.top - margin.bottom;
 
-        this.visualSettings = VisualSettings.parse<VisualSettings>(dataView);
-        this.visualSettings.circle.circleThickness = Math.max(0, this.visualSettings.circle.circleThickness);
-        this.visualSettings.circle.circleThickness = Math.min(10, this.visualSettings.circle.circleThickness);
+        this.svg
+            .attr("width", width)
+            .attr("height", height);
 
-        this.circle
-            .style("fill", this.visualSettings.circle.circleColor)
-            .style("fill-opacity", 0.5)
-            .style("stroke", "black")
-            .style("stroke-width", this.visualSettings.circle.circleThickness)
-            .attr("r", radius)
-            .attr("cx", width / 2)
-            .attr("cy", height / 2);
+        this.container
+            .attr("transform", `translate(${margin.left}, ${margin.top})`)
+            .append("text")
+                .attr("x", innerWidth * 0.5)
+                .attr("y", -5)
+                .text("title")
+                .classed("title", true);
+                
+        this.xAxisContainer
+            .attr("transform", `translate(0, ${innerHeight})`);
+
+        const xScale = d3.scaleLinear()
+            .domain([0, viewModel.maxValue])
+            .range([0, innerWidth]);
+
+        const yScale = d3.scaleBand()
+            .domain(viewModel.dataPoints.map(d => d.category))
+            .range([0, innerHeight])
+            .padding(0.1);
         
-        this.textValue
-            .text(<string>dataView.single.value)
-            .attr("x", "50%")
-            .attr("y", "50%")
-            .attr("dy", "0.35em")
-            .attr("text-anchor", "middle")
-            .style("font-size", fontSizeValue + "px");
+        const xAxis = d3.axisBottom(xScale)
+            .tickFormat(d3.format(".1s"))
+            .tickSize(-innerHeight);
+        const yAxis = d3.axisLeft(yScale);
+    
+        this.xAxisContainer.call(xAxis);
+        this.yAxisContainer.call(yAxis);
+
+        this.xAxisContainer.selectAll(".domain").remove();
+        this.yAxisContainer.selectAll(".domain, .tick line").remove();
+
+        this.xAxisContainer.append("text")
+            .attr("x", innerWidth * 0.5)
+            .attr("y", 30)
+            .attr("fill", "black")
+            .text("x label")
+            .classed("axis-label", true);
         
-        this.textLabel
-            .text(dataView.metadata.columns[0].displayName)
-            .attr("x", "50%")
-            .attr("y", height / 2)
-            .attr("dy", fontSizeValue / 1.2)
-            .attr("text-anchor", "middle")
-            .style("font-size", fontSizeLabel + "px");
+        this.container.selectAll("rect")
+            .data(viewModel.dataPoints).enter()
+            .append("rect")
+                .attr("width", d => xScale(d.value))
+                .attr("height", yScale.bandwidth())
+                .attr("y", d => yScale(d.category))
+                .classed("rect", true);
+    }
+
+    private getViewModel(options: VisualUpdateOptions): ViewModel {
+        let dv = options.dataViews;
+        let viewModel: ViewModel = {
+            dataPoints: [],
+            maxValue: 0
+        };
+
+        if (!dv || !dv[0] || !dv[0].categorical || !dv[0].categorical.categories || !dv[0].categorical.values) {
+            return viewModel;
+        }
+
+        let view = dv[0].categorical;
+        let categories = view.categories[0];
+        let values = view.values[0];
+
+        for (let i = 0; i < Math.max(categories.values.length, values.values.length); i++) {
+            viewModel.dataPoints.push({
+                category: <string>categories.values[i],
+                value: <number>values.values[i]
+            });
+        }
+
+        viewModel.maxValue = d3.max(viewModel.dataPoints, d => d.value);
+        return viewModel;
     }
 
     public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstanceEnumeration {
