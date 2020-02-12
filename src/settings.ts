0288021638479649dcfacfef2971af20566fc188
Jordy Aaldering <aalderingjordy@gmail.com>
2020-02-12 09:02:43 +0100

    Object Settings

--- src/settings.ts
+++ src/settings.ts
@@ -4,13 +4,10 @@ import { dataViewObjectsParser } from "powerbi-visuals-utils-dataviewutils";
 import DataViewObjectsParser = dataViewObjectsParser.DataViewObjectsParser;
 
 export class VisualSettings extends DataViewObjectsParser {
-  public dataPoint: dataPointSettings = new dataPointSettings();
+  public circle: CircleSettings = new CircleSettings();
 }
 
-export class dataPointSettings {
-  public defaultColor: string = "";
-  public showAllDataPoints: boolean = true;
-  public fill: string = "";
-  public fillRule: string = "";
-  public fontSize: number = 12;
+export class CircleSettings {
+  public circleColor: string = "white";
+  public circleThickness: number = 2;
 }
--- src/visual.ts
+++ src/visual.ts
@@ -11,17 +11,20 @@ import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
 
 import DataView = powerbi.DataView;
 import VisualObjectInstance = powerbi.VisualObjectInstance;
-import EnumerateVisualObjectInstancesOptions = powerbi.EnumerateVisualObjectInstancesOptions;
 import VisualObjectInstanceEnumerationObject = powerbi.VisualObjectInstanceEnumerationObject;
 
 import * as d3 from "d3";
+
 import { VisualSettings } from "./settings";
+import VisualObjectInstanceEnumeration = powerbi.VisualObjectInstanceEnumeration;
+import EnumerateVisualObjectInstancesOptions = powerbi.EnumerateVisualObjectInstancesOptions;
 
 type Selection<T extends d3.BaseType> = d3.Selection<T, any, any, any>;
 
 export class Visual implements IVisual {
     private host: IVisualHost;
     private settings: VisualSettings;
+    private visualSettings: VisualSettings;
 
     private svg: Selection<SVGElement>;
     private container: Selection<SVGElement>;
@@ -50,18 +53,24 @@ export class Visual implements IVisual {
 
         this.svg.attr("width", width);
         this.svg.attr("height", height);
-
+        
         let radius: number = Math.min(width, height) / 2.2;
+        let fontSizeValue: number = Math.min(width, height) / 5;
+        let fontSizeLabel: number = fontSizeValue / 4;
+
+        this.visualSettings = VisualSettings.parse<VisualSettings>(dataView);
+        this.visualSettings.circle.circleThickness = Math.max(0, this.visualSettings.circle.circleThickness);
+        this.visualSettings.circle.circleThickness = Math.min(10, this.visualSettings.circle.circleThickness);
+
         this.circle
-            .style("fill", "white")
+            .style("fill", this.visualSettings.circle.circleColor)
             .style("fill-opacity", 0.5)
             .style("stroke", "black")
-            .style("stroke-width", 2)
+            .style("stroke-width", this.visualSettings.circle.circleThickness)
             .attr("r", radius)
             .attr("cx", width / 2)
             .attr("cy", height / 2);
         
-        let fontSizeValue: number = Math.min(width, height) / 5;
         this.textValue
             .text(<string>dataView.single.value)
             .attr("x", "50%")
@@ -70,7 +79,6 @@ export class Visual implements IVisual {
             .attr("text-anchor", "middle")
             .style("font-size", fontSizeValue + "px");
         
-        let fontSizeLabel: number = fontSizeValue / 4;
         this.textLabel
             .text(dataView.metadata.columns[0].displayName)
             .attr("x", "50%")
@@ -80,7 +88,8 @@ export class Visual implements IVisual {
             .style("font-size", fontSizeLabel + "px");
     }
 
-    public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstance[] | VisualObjectInstanceEnumerationObject {
-        return VisualSettings.enumerateObjectInstances(this.settings || VisualSettings.getDefault(), options);
+    public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstanceEnumeration {
+        const settings: VisualSettings = this.visualSettings || <VisualSettings>VisualSettings.getDefault();
+        return VisualSettings.enumerateObjectInstances(settings, options);
     }
 }
