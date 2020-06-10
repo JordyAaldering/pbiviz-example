becc34d1fe24ed6856fa09698cebb2d657f630d0
JordyAaldering <aalderingjordy@gmail.com>
2020-06-10 10:56:42 +0200

    Remove old project

--- src/settings.ts
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-import { DataViewObjectsParser } from "powerbi-visuals-utils-dataviewutils/lib/dataViewObjectsParser";
-
-export class VisualSettings extends DataViewObjectsParser {
-  // Import settings here.
-}
